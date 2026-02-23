26ad93df08842966ed18f3f6076444a88991982d
mattgregory-dev <mattgregory-dev@users.noreply.github.com>
2026-02-23 05:27:19 -0700

    chore: add tailwind with concurently to npm run dev:all

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // Letting sass build it's own files
+    // "./src/**/*.{html,js,scss}",
+    "./**/*.php"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
